ba099265de95146f7fe27f52386a2f0a6f9c4171
wilson <wilson@looptifyapp.com>
2023-06-09 16:25:58 -0700

    testing useauth?

---
+++ src/Dashboard.jsx
@@ -0,0 +1,7 @@
+function Dashboard() {
+    return (
+        <h1>heres some stuff to be protected</h1>
+    )
+}
+
+export default Dashboard;
---
+++ src/Login.jsx
@@ -0,0 +1,11 @@
+function Login() {
+    return (
+        <>
+        <input type='text' placeholder='username'/>
+        <input type='text' placeholder='password'/>
+        <button>login</button>
+        </>
+    )
+}
+
+export default Login;
